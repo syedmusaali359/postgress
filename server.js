b96a3eb277691e01fe5f49705e448ef486bf2375
root <root@ip-10-0-14-228.ec2.internal>
2023-06-01 13:19:38 +0000

    first commit

--- server.js
+++ server.js
@@ -1,33 +1,55 @@
 const express = require('express')
 const morgan = require('morgan')
-const mysql = require('mysql2')
+const { Pool } = require('pg')
 
 const app = express()
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 
 // https://gist.githubusercontent.com/meech-ward/1723b2df87eae8bb6382828fba649d64/raw/ee52637cc953df669d95bb4ab68ac2ad1a96cd9f/lotr.sql
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+const pool = new Pool({
+  host: 'postgress.cjhrqy0ejiqm.us-east-1.rds.amazonaws.com',
+  user: 'postgress',
+  port: 5432,
+  password: 'postgress',
+  database: 'postgress',
 })
+
+
+// Check PostgreSQL connection
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
+  }
+  console.log('Connected to PostgreSQL')
+  release()
+})
+
+// Test route to check database connection
+app.get("/", async (req, res) => {
+  try {
+    const query = 'SELECT 1';
+    const result = await pool.query(query);
+    res.send({ status: 'Connected to PostgreSQL' });
+  } catch (error) {
+    console.error('Error querying database:', error.stack);
+    res.status(500).send({ error: 'Failed to connect to PostgreSQL' });
+  }
+});
 
 function getRandomInt(max) {
   return 1 + Math.floor(Math.random() * (max-1))
 }
 
 async function getCharacter(id) {
-  const [characters] = await pool.promise().query("SELECT * FROM characters WHERE id = ?", [
-    id,
-  ])
+  const query = 'SELECT * FROM characters WHERE id = $1';
+  const { rows } = await pool.query(query, [id]);
+  const characters = rows;
   return characters[0]
 }
 async function randomId() {
-  const [rows] = await pool.promise().query(
-    "SELECT COUNT(*) as totalCharacters FROM characters"
-  )
+  const query = 'SELECT COUNT(*) as totalCharacters FROM characters';
+  const { rows } = await pool.query(query);
   const { totalCharacters } = rows[0]
   const randomId = getRandomInt(totalCharacters)
   return randomId
@@ -37,15 +59,6 @@ app.get("/test", (req, res) => {
   res.send("<h1>It's working 🤗</h1>")
 })
 
-app.get("/", async (req, res) => {
-  try {
-    const id = await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
-  } catch (error) {
-    res.send(error)
-  }
-})
 
 app.get("/:id", async (req, res) => {
   try {
